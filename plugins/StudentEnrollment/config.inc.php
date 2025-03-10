08747020bdb4d46c898d858bd0a5f05d9cfdb279
mohamedS16 <mohamedsameh162000@gmail.com>
2025-03-11 00:15:38 +0200

    studentEnrollment plugin commit

---
+++ plugins/StudentEnrollment/config.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+
+if ( empty( $_REQUEST['save'] )
+	&& empty( $_REQUEST['missing_attendance'] ) )
+{
+	$res = getEnrollmentReport();
+	echo '
+	<a href="#?h=1">cclicj</a>
+	<table border="1">
+    <thead>
+        <tr>
+            <th>STUDENT</th>
+            <th>Course</th>
+            <th>SUBJECT</th>
+        </tr>
+    </thead>
+    <tbody>';
+	foreach($res as $r){
+		echo "
+        <tr>
+            <td> " . $r['STUDENT_NAME'] . "</td>
+            <td>" . $r['COURSE_NAME'] . "</td>
+            <td>" . $r['SUBJECT_NAME'] . "</td>
+        </tr>";
+	}
+    echo '</tbody>
+</table>
+';
+}
---
+++ plugins/StudentEnrollment/functions.php
@@ -0,0 +1,23 @@
+<?php 
+
+function getEnrollmentReport(){
+	$qery = "select s.username as student_name, c.title as course_name, cs.title as subject_name from student_enrollment se 
+			left join students s on se.student_id = s.student_id 
+			left join schedule sch on se.student_id = sch.student_id 
+			left join courses c on sch.course_id = c.course_id 
+			left join course_subjects cs on c.subject_id = cs.subject_id";
+
+	$res = DBGet($qery);
+
+	return $res;
+}
+
+add_action( 'Warehouse.php|header_head', 'reportStyle' );
+
+
+function reportStyle()
+{
+	echo '<link rel="stylesheet" href="plugins/StudentEnrollment/style.css" />';
+}
+
+?>
